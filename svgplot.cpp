a3908a8dc83a7b883ec790ba22a3494615f39270
rhoumi <remi.georges83@gmail.com>
2023-06-19 16:41:36 +0200

    from main to class

--- svgplot.cpp
+++ svgplot.cpp
@@ -9,7 +9,7 @@
 
 /************************************************************************
  FAUST Architecture File
- Copyright (C) 2003-2019 GRAME, Centre National de Creation Musicale
+ Copyright (C) 2003-2023 GRAME, Centre National de Creation Musicale
  ---------------------------------------------------------------------
  This Architecture section is free software; you can redistribute it
  and/or modify it under the terms of the GNU General Public License
@@ -33,6 +33,8 @@
  ************************************************************************/
 
 
+#include <cstdio>
+#include <faust/gui/UI.h>
 #include <iostream>
 #include <string>
 
@@ -98,6 +100,104 @@ struct DisplayUI : public GenericUI {
     
 };
 
+
+class faust2svgplot {
+
+    private:
+        
+        FAUSTFLOAT** DSP_inputs;
+        FAUSTFLOAT** DSP_outputs;
+        dsp* DSP;
+        FAUSTFLOAT nsamples;
+        FAUSTFLOAT srate;
+        signalsmith::plot::Plot2D plot;
+
+    public : 
+        
+        FAUSTFLOAT** createbuffer (int chan, int buffer_size)
+        {
+            
+            FAUSTFLOAT** buffer = new FAUSTFLOAT*[chan];
+                for (int i = 0; i < chan; i++) {
+                    buffer[i] = new FAUSTFLOAT[buffer_size];
+                    memset(buffer[i], 0, sizeof(FAUSTFLOAT) * buffer_size);
+                }
+                return buffer;
+        }
+
+        void deletebuffer (FAUSTFLOAT** buffer, int chan)
+        {
+                for (int i = 0; i <chan; i++) {
+                    delete [] buffer[i];
+                }
+                delete [] buffer;
+        }
+
+        faust2svgplot(dsp* dsp, FAUSTFLOAT sample_rate, FAUSTFLOAT nb_samples)
+        {    
+
+            DSP = dsp; 
+            nsamples= nb_samples;
+            srate = sample_rate;
+            DSP_inputs = createbuffer(DSP->getNumInputs(), nsamples);
+            DSP_outputs = createbuffer(DSP->getNumOutputs(),nsamples);
+            //init signal processor and the user interface values
+            DSP->init(sample_rate);
+        }
+
+
+        void exec()
+        {       
+            
+            DSP->compute(nsamples, DSP_inputs, DSP_outputs);
+
+            //init position for max n min
+            FAUSTFLOAT max = DSP_outputs[0][0]; 
+            FAUSTFLOAT min =  DSP_outputs[0][0]; 
+            //go through the outputs buffer
+            for (int chan=0; chan< DSP->getNumOutputs(); ++chan) 
+                 {
+                //reset for straight line and no dotted line
+                plot.styleCounter.dash = 0;
+                //create a line for the actual channel
+                auto &line = plot.line();
+                //fixed Index colour position similar to channel
+                line.styleIndex.colour = chan; 
+                for (int frame=0; frame < nsamples; ++frame) 
+                    { 
+                        std::cout << "frame: "<< frame;
+                        FAUSTFLOAT* sub_outputs = DSP_outputs[chan];
+                        std::cout << " | Channel " << chan+1 << " :" << sub_outputs[frame] << "\t";
+                        std::cout << std::endl;
+                        //write points in line
+                        line.add(frame, sub_outputs[frame]);
+                        //check for max and min in order to create proportional axes
+                        
+                        if (max <= sub_outputs[frame]) 
+                        {
+                        max = sub_outputs[frame];
+                        }
+                        if (min >= sub_outputs[frame]) 
+                        {
+                        min = sub_outputs[frame];
+                        }  
+                    }
+                //add label name
+                line.label("Channel "+std::to_string(chan));
+                }
+        plot.x.linear(0,nsamples).major(0).minor(nsamples).label("Samples");
+	    plot.y.minors(min, max).label("Values");
+        plot.y.majors(0);
+        plot.write("mydsp.svg");
+        }
+
+        virtual ~faust2svgplot()
+        {
+            deletebuffer(DSP_inputs, DSP->getNumInputs());
+            deletebuffer(DSP_outputs, DSP->getNumOutputs());
+        } 
+};
+
 /******************************************************************************
  *******************************************************************************
  
@@ -119,32 +219,11 @@ struct DisplayUI : public GenericUI {
 /*******************BEGIN ARCHITECTURE SECTION (part 2/2)***************/
 
 
-
- 
-dsp* DSP;
-
 std::list<GUI*> GUI::fGuiList;
 ztimedmap GUI::gTimedZoneMap;
 
-//#define kFrames 512
-//
-//signalsmith::plot::PlotStyle customStyle();
-//
 signalsmith::plot::Plot2D plot;
 
-
-//Tried to configure style but don't know how it works, I hard configure the plot.h fo the moment
-//
-//signalsmith::plot::PlotStyle customStyle(){
-//signalsmith::plot::PlotStyle style;
-//style.lineWidth = 0.1;
-//style.valueSize = 1;
-//style.labelSize=2;
-//style.fontAspectRatio = 0.1;
-//return style;
-//};
-//
-//
 int main(int argc, char* argv[])
 {
     dsp* DSP = new mydsp();
@@ -158,7 +237,6 @@ int main(int argc, char* argv[])
     //maybe for future addons
     //interface->addOption("-bs", &buffer_size, kFrames, 0.0, kFrames * 16);
     //interface->addOption("-s", &start_at_sample, 0, 0.0, 100000000.0);
-
     
     if (DSP->getNumInputs() > 0) {
         cerr << "no inputs allowed " << endl;
@@ -169,7 +247,8 @@ int main(int argc, char* argv[])
     interface->process_one_init("-r");
     
     // init signal processor and the user interface values
-    DSP->init(sample_rate);
+ 
+    faust2svgplot* f2svg= new faust2svgplot(DSP, sample_rate, nb_samples);
     
     // modify the UI values according to the command line options, after init
     interface->process_init();
@@ -182,70 +261,10 @@ int main(int argc, char* argv[])
     DisplayUI disp;
     DSP->buildUserInterface(&disp);
     
-    //creating inputs & outputs buffers
+    f2svg->exec();
 
-    FAUSTFLOAT** DSP_inputs = new FAUSTFLOAT*[DSP->getNumInputs()];
-        for (int i = 0; i<DSP->getNumInputs(); i++){
-                DSP_inputs[i] = new FAUSTFLOAT[int(nb_samples)];
-                memset(DSP_inputs[i],0,sizeof(FAUSTFLOAT) *nb_samples);
-                }
-
-    FAUSTFLOAT** DSP_outputs = new FAUSTFLOAT*[DSP->getNumOutputs()];
-        for (int i = 0; i<DSP->getNumOutputs(); i++){
-                DSP_outputs[i] = new FAUSTFLOAT[int(nb_samples)];
-                memset(DSP_outputs[i],0,sizeof(FAUSTFLOAT) *nb_samples);
-                }
-    
-    //compute into inputs and outputs
-    DSP->compute(nb_samples, DSP_inputs, DSP_outputs);
-
-    //init position for max n min
-    FAUSTFLOAT max = DSP_outputs[0][0]; 
-    FAUSTFLOAT min =  DSP_outputs[0][0]; 
-    //go through the outputs buffer
-    for (int chan=0; chan< DSP->getNumOutputs(); ++chan) 
-        {
-        //reset for straight line and no dotted line
-        plot.styleCounter.dash = 0;
-        //create a line for the actual channel
-        auto &line = plot.line();
-        //fixed Index colour position similar to channel
-        line.styleIndex.colour = chan; 
-        for (int frame=0; frame < nb_samples; ++frame) 
-            { 
-                std::cout << "frame: "<< frame;
-                FAUSTFLOAT* sub_outputs = DSP_outputs[chan];
-                std::cout << " | Channel " << chan+1 << " :" << sub_outputs[frame] << "\t";
-                std::cout << std::endl;
-                //write points in line
-                line.add(frame, sub_outputs[frame]);
-              //check for max and min in order to create proportional axes
-              if (max <= sub_outputs[frame]) {
-              max = sub_outputs[frame];
-              }
-              if (min >= sub_outputs[frame]) {
-              min = sub_outputs[frame];
-              }  
-            }
-        //add label name
-        line.label("Channel "+std::to_string(chan));
-        }
- 
-    plot.x.linear(0,nb_samples).major(0).minor(nb_samples).label("Samples");
-	plot.y.minors(min, max).label("Values");
-    plot.y.majors(0);
-
-    //auto style = plot.defaultStyle();
-    //style.fontAspectRatio = 0.5;
-    
-    ////Logo Faust not working because not proportional to the axes, i tried but it's maybe a useless feature 
-    //plot.image({0, nb_samples/8,min-2*min,max-max/2}, "https://faust.grame.fr/community/logos/img/LOGO_FAUST_SIMPLE_BLEU.png");
-
-    plot.write("mydsp.svg");
-    delete[] DSP_inputs;
-    delete[] DSP_outputs;
+    delete f2svg;
     delete DSP;
-    return 0;
 }
 
 /******************** END svgplot.cpp ****************/
